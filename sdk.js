7f51faa2e3d361367e61395298ccb796ebd4dfaa
Warat Wongmaneekit <canopybanj@gmail.com>
2021-06-28 16:04:59 +0700

    Update sdk

--- sdk.js
+++ sdk.js
@@ -2,6 +2,7 @@ class Warroom {
 
     constructor() {
         this.appType = ""
+        this.token = ""
         this.isReady = false
         document.addEventListener('warroom-miniapp:ready-state-changed', (e) => {
             resolve(true)
@@ -10,26 +11,35 @@ class Warroom {
     
     // This is util for sending event back to Warroom
     postMessageToParent (eventName, payload) {
-        parent.postMessage({'warroom-miniapp-type': this.appType, event: eventName, payload:payload }, "*")
+        if(this.token === "") {
+            throw Error('Authentication is not valid please init app before use')
+        } else {
+            parent.postMessage({'warroom-miniapp-type': this.appType, event: eventName, payload:payload, token: this.token }, "*")
+        }
     }
 
     // Handle method when WARROOM send message back to Mini App
     handleMessageFromWarroom (event) {
         if(event.data.provider === 'warroom') {
-            switch(event.data.event) {
-                case 'response-to:status:init':
-                    document.dispatchEvent(new CustomEvent("warroom-miniapp:status:init", { detail: event.data.payload }));
-                    break;
-                case 'response-to:get:current-thead':
-                    // (GET) Step 4: Recieve message from WARROOM and create event for Mini App
-                    document.dispatchEvent(new CustomEvent("warroom-miniapp:get:current-thead", { detail: event.data.payload }));
-                    break;
-                case 'send:case:on-close-case':
-                        // (EVENT) Step 3: Recieve message from WARROOM and create event for Mini App
-                        document.dispatchEvent(new CustomEvent("warroom-miniapp:case:on-close-case", { detail: event.data.payload }));
+
+            if (event.data.token !== this.token || this.token === "") {
+                throw Error('Authentication is not valid please init app before use')
+            } else {
+                switch(event.data.event) {
+                    case 'response-to:status:init':
+                        document.dispatchEvent(new CustomEvent("warroom-miniapp:status:init", { detail: event.data.payload }));
                         break;
-                default:
-                    
+                    case 'response-to:get:current-thead':
+                        // (GET) Step 4: Recieve message from WARROOM and create event for Mini App
+                        document.dispatchEvent(new CustomEvent("warroom-miniapp:get:current-thead", { detail: event.data.payload }));
+                        break;
+                    case 'send:case:on-close-case':
+                            // (EVENT) Step 3: Recieve message from WARROOM and create event for Mini App
+                            document.dispatchEvent(new CustomEvent("warroom-miniapp:case:on-close-case", { detail: event.data.payload }));
+                            break;
+                    default:
+                        
+                }
             }
 
             console.log('[MINI APP]',event.data)
@@ -47,7 +57,14 @@ class Warroom {
             }
 
             document.addEventListener('warroom-miniapp:status:init', (e) => {
-                resolve(true)
+                // Get recieve from warroom  Verify token with end point
+                
+                if(true) {
+                    // Save token when valid
+                    this.token = "abcdefg"
+                    resolve(true)
+                }
+
             });
         })
     }

6cca7ab602c68936e6973b4ff646259b9dc33ff1
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2022-08-18 15:11:31 +0700

    Update to support firebase deploy

---
+++ functions/index.js
@@ -0,0 +1,16 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.static("dist"));
+app.get("/", (req, res) => {
+  res.sendFile("public/warroom.html", {root: __dirname});
+});
+
+app.get("/miniapp", (req, res) => {
+  res.sendFile("public/miniapp.html", {root: __dirname});
+});
+
+exports.app = functions.https.onRequest(app);
